b646f246c2bdf8446e920596b9060eae1230d0db
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2022-11-07 18:35:03 +0530

    added Leetcode Dice throw  problem

---
+++ LeetCode/Dice.java
@@ -0,0 +1,19 @@
+//Return the combinations of dice throw required to get the target value
+
+public class Dice{
+    public static void main(String[] args) {
+        dice("",4);
+    }
+
+    static void dice(String p, int target){
+        if(target == 0){
+            System.out.println(p);
+            return ;
+        }
+
+        for (int i = 1; i <= 6 && i<=target; i++) {
+            dice(p+i,target-i);
+        }
+
+        }
+    }
