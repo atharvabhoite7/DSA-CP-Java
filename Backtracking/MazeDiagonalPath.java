114e0d962de0ba84ac572a58e9f70973a94b6c64
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2022-11-08 22:56:04 +0530

    added Diagonal Path Maze problem for Backtracking

--- Backtracking/MazeDiagonalPath.java
+++ Backtracking/MazeDiagonalPath.java
@@ -1,5 +1,5 @@
-//Return the path required to reach from point A to B in a Maze by going down and right only
-public class MazePath {
+//Return the path required to reach from point A to B in a Maze by going down, right and diagonal as well
+public class MazeDiagonalPath {
     public static void main(String[] args) {
         path("", 3, 3);
     }
@@ -11,12 +11,15 @@ public class MazePath {
         }
 
         if (r > 1) {
-            path(p + 'D', r - 1, c);
+            path(p + 'V', r - 1, c);
         }
 
         if (c > 1) {
-            path(p + 'R', r, c - 1);
+            path(p + 'H', r, c - 1);
         }
 
+        if (r > 1 && c > 1) {
+            path(p + 'D', r - 1, c - 1);
+        }
     }
 }
