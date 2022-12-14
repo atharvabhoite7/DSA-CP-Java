295b4584aeda8631b7b2576eeab9328e69bf95dc
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2022-12-14 19:13:50 +0530

    added solution to Prob 198 of leetcode

---
+++ LeetCode/Prob198.java
@@ -0,0 +1,14 @@
+// Solution to problem number 198 of Leetcode
+
+class Solution {
+    public int rob(int[] nums) {
+        int[] memo = new int [nums.length+1];
+        memo[0]=0;
+        memo[1]=nums[0];
+        for(int i=1; i<nums.length; i++){
+            int val = nums[i];
+            memo[i+1]=Math.max(memo[i], memo[i-1]+val);
+        }
+        return memo[nums.length];
+    }
+}
