7bdb0ba0b60d416e8c9a3fd39ca2c6192fdb81db
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2023-01-15 12:42:18 +0530

    added print root to leaf in a BST

--- Trees/BST.java
+++ Trees/BST.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class BST {
     static class Node {
         int data;
@@ -97,6 +99,29 @@ public class BST {
         }
     }
 
+    public static void printPath(ArrayList<Integer> path) {
+        for (int i = 0; i < path.size(); i++) {
+            System.out.print(path.get(i) + "->");
+        }
+        System.out.println();
+    }
+
+    public static void printRoot2Leaf(Node root, ArrayList<Integer> path) {
+        if (root == null) {
+            return;
+        }
+
+        path.add(root.data);
+
+        if (root.left == null && root.right == null) {
+            printPath(path); // leaf
+        } else {
+            printRoot2Leaf(root.left, path);
+            printRoot2Leaf(root.right, path);
+        }
+        path.remove(path.size() - 1);
+    }
+
     public static void main(String[] args) {
         int values[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
         Node root = null;
@@ -108,16 +133,17 @@ public class BST {
         inorder(root);
         System.out.println();
 
-        // if (search(root, 0)) {
-        // System.out.println("found");
-        // } else {
-        // System.out.println("not found");
-        // }
+        if (search(root, 0)) {
+            System.out.println("found");
+        } else {
+            System.out.println("not found");
+        }
 
-        // delete(root, 5);
-        // inorder(root);
+        delete(root, 5);
+        inorder(root);
 
         printInRange(root, 4, 12);
+        printRoot2Leaf(root, new ArrayList<>());
     }
 
 }
