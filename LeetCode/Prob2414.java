ef8f9a81957a5afc3dea9548c6855b2c4e0b4d3e
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2023-01-25 00:01:48 +0530

    added solution to Prob 2413 of Leetcode

---
+++ LeetCode/Prob2414.java
@@ -0,0 +1,21 @@
+// Solution to problem number 2414 of Leetcode
+
+class Solution {
+    public int longestContinuousSubstring(String s) {
+        int count = 0;
+        int curr = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            char ch = s.charAt(i);
+            char ch2 = s.charAt(i + 1);
+            if ((int) ch + 1 == (int) ch2) {
+                count++;
+                if (count > curr) {
+                    curr = count;
+                }
+            } else {
+                count = 0;
+            }
+        }
+        return curr + 1;
+    }
+}
