47947231c24be55654e70472c324fb36359ef46c
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2023-02-27 11:39:01 +0530

    added solution to Prob 1991 of Leetcode

---
+++ LeetCode/Prob1991.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        int sumI = 0;
+        int sumJ = 0;
+        int[] sumLeft = new int[nums.length];
+        int[] sumRight = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            sumI += nums[i];
+            sumLeft[i] = sumI;
+        }
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            sumJ += nums[i];
+            sumRight[i] = sumJ;
+        }
+
+        for (int i = 0; i <= nums.length - 1; i++) {
+            if (sumRight[i] == sumLeft[i]) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
