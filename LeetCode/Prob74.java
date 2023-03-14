abf65f6d60ba041687b3dd2141c32b2344d9705c
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2023-03-14 20:44:41 +0530

    added solution to Prob 74 of Leetcode

--- LeetCode/Prob74.java
+++ LeetCode/Prob74.java
@@ -1,13 +1,33 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
 
-        for (int row = 0; row < matrix.length; row++) {
-            for (int col = 0; col < matrix[row].length; col++) {
-                if (target == matrix[row][col]) {
-                    return true;
+        // brute force
+        // int col = 0;
+        // for(int row=0; row<matrix.length; row++){
+        // for(col=0; col<matrix[row].length; col++){
+        // if(target == matrix[row][col]){
+        // return true;
+        // }
+        // }
+        // }
+        // return false;
+
+        // optimised soln
+        int row = 0;
+        int col = matrix[0].length - 1;
+
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else {
+                if (target > matrix[row][col]) {
+                    row++;
+                } else {
+                    col--;
                 }
             }
         }
         return false;
+
     }
 }
