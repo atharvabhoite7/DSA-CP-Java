0dc6c8896cbbf9008c606d420eb466007a23b2e5
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2023-04-02 18:25:48 +0530

    added solution to Prob 442 of Leetcode

---
+++ LeetCode/Prob442.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        int[] count = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            count[nums[i]]++;
+        }
+
+        for (int i = 1; i < count.length; i++) {
+            if (count[i] == 2) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
