6138b4821cb838df2c4597858d9fa1f3fc5c15e8
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2023-04-03 20:27:41 +0530

    added solution to Prob 56 of Leetcode

---
+++ LeetCode/Prob56.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        LinkedList<int[]> list = new LinkedList<>();
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        for (int[] interval : intervals) {
+            if (list.isEmpty() || list.getLast()[1] < interval[0]) {
+                list.add(interval);
+            } else {
+                list.getLast()[1] = Math.max(list.getLast()[1], interval[1]);
+            }
+        }
+        return list.toArray(new int[list.size()][]);
+    }
+
+}
