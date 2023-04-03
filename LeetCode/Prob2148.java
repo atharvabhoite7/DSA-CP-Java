c0c229b55de3ebf16174d247106c696b74d1e0c3
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2023-04-03 23:45:52 +0530

    added solution to Prob 2148 of Leetcode

---
+++ LeetCode/Prob2148.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countElements(int[] nums) {
+        Arrays.sort(nums);
+        int s = nums[0];
+        int e = nums[nums.length - 1];
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != s && nums[i] != e) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
