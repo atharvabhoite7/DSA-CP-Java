994a046676defefd0dc022c0408f1f2618a5568c
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2023-05-29 00:02:16 +0530

    added Prob 1547 of leetcode

---
+++ LeetCode/Prob1547.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int minCost(int n, int[] cuts) {
+        Arrays.sort(cuts);
+
+        int m = cuts.length;
+        int[][] dp = new int[m][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                dp[i][j] = -1;
+            }
+        }
+
+        return func(0, m - 1, 0, n, cuts, dp);
+    }
+
+    public int func(int start, int end, int rodStart, int rodEnd, int[] cuts, int[][] dp) {
+        if (end < start) {
+            return 0;
+        }
+
+        if (dp[start][end] != -1) {
+            return dp[start][end];
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for (int i = start; i <= end; i++) {
+            int cost = rodEnd - rodStart +
+                    func(start, i - 1, rodStart, cuts[i], cuts, dp) +
+                    func(i + 1, end, cuts[i], rodEnd, cuts, dp);
+
+            ans = Math.min(cost, ans);
+        }
+        dp[start][end] = ans;
+        return ans;
+
+    }
+}
