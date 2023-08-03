35bd9e0aa4746df7cf64edfacf55d2aed9cab422
Atharva Bhoite <2020.atharva.bhoite@ves.ac.in>
2023-08-03 20:06:43 +0530

    added Prob 1004 of Leetcode

---
+++ LeetCode/Prob1004.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int size = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                k--;
+            }
+
+            if (k >= 0) {
+                size++;
+            } else if (nums[i - size] == 0) {
+                k++;
+            }
+        }
+        return size;
+    }
+}
